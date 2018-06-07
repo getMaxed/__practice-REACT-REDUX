59a217e4a9119b2c5bb4f455b8f9a038f1e86729
getMaxed <maxfaden07@gmail.com>
2018-06-07 06:41:49 -0700

    posts-api-playground_project: Decouple components. Add user fetching

--- posts-api-playground/src/components/App.jsx
+++ posts-api-playground/src/components/App.jsx
@@ -6,26 +6,33 @@ class App extends Component {
         this.state = {
             posts: [],
             numberEntered: null,
-            showingPost: null
+            showingPost: null,
+            showingUser: null,
+            postView: true
         };
     }
 
     componentDidMount() {
-        const url = 'https://jsonplaceholder.typicode.com/posts';
-        fetch(url)
+        const postsURL = 'https://jsonplaceholder.typicode.com/posts';
+        fetch(postsURL)
             .then(res => res.json())
             .then(posts => {
                 const randomNumber = Math.floor(Math.random() * 100);
                 const randomPost = posts.find(post => post.id === randomNumber);
-                this.setState({
-                    posts: posts,
-                    showingPost: randomPost
+                fetch(
+                    `https://jsonplaceholder.typicode.com/users/${
+                        randomPost.userId
+                    }`
+                ).then(res => {
+                    res.json().then(user => {
+                        this.setState({
+                            posts: posts,
+                            showingPost: randomPost,
+                            showingUser: user
+                        });
+                    });
                 });
             });
-    }
-
-    componentDidUpdate(prevState) {
-        console.log(prevState);
     }
 
     handleNumberEntered = number => {
@@ -34,13 +41,34 @@ class App extends Component {
         });
     };
 
+    handleOpenUserView = () => {
+        this.setState({
+            postView: false
+        });
+    };
+
+    handleGoBack = () => {
+        this.setState({
+            postView: true
+        });
+    };
+
     render() {
         return (
             <div>
                 {this.state.posts.length !== 0 && (
                     <div className="container">
-                        <Header post={this.state.showingPost} />
-                        <Main post={this.state.showingPost} />
+                        <Header
+                            post={this.state.showingPost}
+                            user={this.state.showingUser}
+                            openUserView={this.handleOpenUserView}
+                        />
+                        <Main
+                            post={this.state.showingPost}
+                            user={this.state.showingUser}
+                            postView={this.state.postView}
+                            goBack={this.handleGoBack}
+                        />
                         <Footer numberEntered={this.handleNumberEntered} />
                     </div>
                 )}
@@ -58,12 +86,54 @@ class App extends Component {
 const Header = props => {
     return (
         <div className="header">
-            <div>
-                <h4 className="left">Post number : {props.post.id}</h4>
-            </div>
-            <div>
-                <h4 className="right">Written by : {props.post.userId}</h4>
-            </div>
+            <PostNumber postNumber={props.post.id} />
+            <PostAuthor
+                postAuthor={props.user}
+                openUserView={props.openUserView}
+            />
+        </div>
+    );
+};
+
+/*
+|--------------------------------------------------------------------------
+| PostNumber Component
+|--------------------------------------------------------------------------
+*/
+
+const PostNumber = props => {
+    return (
+        <div>
+            <h4 className="left">Post number : {props.postNumber}</h4>
+        </div>
+    );
+};
+
+/*
+|--------------------------------------------------------------------------
+| PostAuthor Component
+|--------------------------------------------------------------------------
+*/
+
+const PostAuthor = props => {
+    let name = props.postAuthor.name.slice();
+
+    if (name.length > 21) {
+        const nameArr = name.split(' ');
+        const firstName = nameArr[0].charAt(0).concat('.');
+        const lastName = nameArr.slice(1);
+        return (name = firstName + ' ' + lastName);
+        console.log(name);
+    }
+
+    return (
+        <div>
+            <h4 className="right">
+                Author:&nbsp;
+                <span onClick={props.openUserView} className="author">
+                    {name}
+                </span>
+            </h4>
         </div>
     );
 };
@@ -75,10 +145,56 @@ const Header = props => {
 */
 
 const Main = props => {
+    const title = props.postView ? props.post.title : props.user.name;
+    const body = props.postView ? props.post.body : props.user;
+
     return (
         <div className="Main">
-            <h2>{props.post.title}</h2>
-            <p>{props.post.body}</p>
+            <MainTitle title={title} />
+            <MainBody
+                body={body}
+                postView={props.postView}
+                goBack={props.goBack}
+            />
+        </div>
+    );
+};
+
+/*
+|--------------------------------------------------------------------------
+| MainTitle Component
+|--------------------------------------------------------------------------
+*/
+
+const MainTitle = props => {
+    return <h2>{props.title}</h2>;
+};
+
+/*
+|--------------------------------------------------------------------------
+| MainBody Component
+|--------------------------------------------------------------------------
+*/
+
+const MainBody = props => {
+    return (
+        <div>
+            {props.postView ? (
+                <p>{props.body}</p>
+            ) : (
+                <div>
+                    <ul>
+                        <li>Name: {props.body.name}</li>
+                        <li>Phone: {props.body.phone}</li>
+                        <li>Username: {props.body.username}</li>
+                        <li>Website: {props.body.website}</li>
+                    </ul>
+                    <br />
+                    <button onClick={props.goBack} className="back">
+                        Back to post
+                    </button>
+                </div>
+            )}
         </div>
     );
 };
@@ -108,18 +224,24 @@ class Footer extends Component {
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <input
-                    type="number"
-                    min="0"
-                    max="100"
-                    value={this.state.formInput}
-                    onChange={this.handleChange}
-                />
-                <br />
-                <br />
-                <input type="submit" value="submit" />
-            </form>
+            <div className="footer">
+                <form>
+                    <label htmlFor="">Enter a number from 1 to 100 :</label>
+                    &nbsp;&nbsp;
+                    <div className="submitNumber">
+                        <input
+                            type="number"
+                            min="0"
+                            max="100"
+                            value={this.state.formInput}
+                            onChange={this.handleChange}
+                        />&nbsp;&nbsp;
+                        <button type="submit" onClick={this.handleSubmit}>
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </div>
         );
     }
 }
